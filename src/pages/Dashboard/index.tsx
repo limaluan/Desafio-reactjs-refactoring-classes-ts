25ef8c85dd3cdcebcbcbaf1d3a2370a8f1774990
limaluan <limaluan.dev@gmail.com>
2022-09-08 21:18:29 -0300

    fix: imports Header on Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 
-import Header from "../../components/Header";
+import { Header } from "../../components/Header";
 import api from "../../services/api";
 import { Food } from "../../components/Food";
 import ModalAddFood from "../../components/ModalAddFood";
 import {
-  
-  IEditingFoodProps, ModalEditFood,
+  IEditingFoodProps,
+  ModalEditFood,
 } from "../../components/ModalEditFood";
 import { FoodsContainer } from "./styles";
 
