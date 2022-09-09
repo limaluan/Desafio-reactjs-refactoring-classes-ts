3dea4133adfd36c89dafb594116f76903a14f6ae
limaluan <limaluan.dev@gmail.com>
2022-09-08 22:18:24 -0300

    refactor: converts some files to typescript /4

--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -1,53 +1,41 @@
-import { createRef } from "react";
+import { useRef } from "react";
 import { FiCheckSquare } from "react-icons/fi";
 
 import { Form } from "./styles";
 import { Modal } from "../Modal";
 import { Input } from "../Input";
 import { IFoodProps } from "../../pages/Dashboard";
-import { FormHandles } from "@unform/core";
 
-export interface IEditingFoodProps {
-  id: number;
-  image: string;
-  name: string;
-  price: string;
-  description: string;
-}
-
-interface IModalEditFoodProps {
-  isOpen: boolean;
+interface IModalAddFood {
   setIsOpen: () => void;
-  editingFood: IEditingFoodProps;
-  handleUpdateFood: (data: IFoodProps) => void;
+  isOpen: boolean;
+  handleAddFood: (food: IFoodProps) => void;
 }
 
-export function ModalEditFood({
-  editingFood,
-  handleUpdateFood,
+export function ModalAddFood({
   isOpen,
+  handleAddFood,
   setIsOpen,
-}: IModalEditFoodProps) {
-  const formRef = createRef<FormHandles>();
-
+}: IModalAddFood) {
+  const formRef = useRef(null);
+  
   const handleSubmit = async (data: IFoodProps) => {
-    handleUpdateFood(data);
+    handleAddFood(data);
     setIsOpen();
   };
 
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
-        <h1>Editar Prato</h1>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <h1>Novo Prato</h1>
         <Input name="image" placeholder="Cole o link aqui" />
 
         <Input name="name" placeholder="Ex: Moda Italiana" />
         <Input name="price" placeholder="Ex: 19.90" />
 
         <Input name="description" placeholder="Descrição" />
-
-        <button type="submit" data-testid="edit-food-button">
-          <div className="text">Editar Prato</div>
+        <button type="submit" data-testid="add-food-button">
+          <p className="text">Adicionar Prato</p>
           <div className="icon">
             <FiCheckSquare size={24} />
           </div>
